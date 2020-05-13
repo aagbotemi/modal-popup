953293662627913df6e8ddd5215baae00a79a875
Abiodun Awoyemi <63763418+gbotemi-tech@users.noreply.github.com>
2020-05-13 17:31:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const modal = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+    modal.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+    modal.classList.remove("show");
+});
